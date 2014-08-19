79187d3f78627264024f14b73455ce6c98214974
moyoron64 <daix203440@gamil.com>
2014-08-19 22:33:29 +0900

    jump

--- Unity/Assets/Scripts/Player.cs
+++ Unity/Assets/Scripts/Player.cs
@@ -5,6 +5,16 @@ using System.Collections;
 [RequireComponent(typeof(BoxCollider2D))]
 public class Player : MonoBehaviour {
 	
+	public int jumpForce = 1;
+	
+	private bool jump = false;
+	private bool isGrounded  = true ;
+	float lastZ;
+
+	void Start(){
+				this.lastZ = 0;
+		}
+
 
 	public Rigidbody2D cRigidbody2D
 	{
@@ -22,13 +32,63 @@ public class Player : MonoBehaviour {
 	void FixedUpdate()
 	{
 		Move();
+		Jump();
+
+		float g = Input.acceleration.magnitude - 1.0f;
+		lastZ = (lastZ + g) * 0.9f;
+
+
+		
+		/*if (Input.acceleration.x > 0.5f || Input.acceleration.x < 0.3f )
+						return;*/
+
+		
+		if ( lastZ > 0.7f && jump == false) {
+						jump = true;
+						Debug.Log("spaceon");
+		} 
+		/*if (jump == true) {
+			cRigidbody2D.velocity = new Vector2(1000000,
+			                                    100000000000000);
+		
+		}*/
+
+
+
 	}
+
+
+	void Jump()
+	{
+		if(jump == true &&  isGrounded)
+		{
+			rigidbody2D.AddForce(Vector2.up * 600f);
+			Debug.Log("spaceon");
+			isGrounded=false;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if(col.gameObject.tag == "yuka")
+			isGrounded = true;
+			jump = false;
+		Debug.Log("spaceonaaaaaaaaaaa");
+	}
+
+
+
 
 
 
 	void Move()
 	{
-		cRigidbody2D.velocity = new Vector2(moveSpeed * Input.acceleration.x,
+			cRigidbody2D.velocity = new Vector2(moveSpeed * Input.acceleration.x,
 		                                    cRigidbody2D.velocity.y);
+		
+
+
+
+
 	}
 }
