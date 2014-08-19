108ce07ff7c95e6ec646f91cebdec9a114319d7d
moyoron64 <daix203440@gamil.com>
2014-08-19 19:08:16 +0900

    make

---
+++ Unity/Assets/Player.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class Player : MonoBehaviour {
+	
+	public Rigidbody2D cRigidbody2D
+	{
+		get
+		{
+			if(!_cRigidbody2D)
+				_cRigidbody2D = rigidbody2D;
+			return _cRigidbody2D;
+		}
+	}
+	Rigidbody2D _cRigidbody2D;
+	
+	public float moveSpeed = 5;
+	
+	void FixedUpdate()
+	{
+		Move();
+	}
+	
+	void Move()
+	{
+		cRigidbody2D.velocity = new Vector2(moveSpeed * Input.GetAxisRaw("Horizontal"),
+		                                    cRigidbody2D.velocity.y);
+	}
+}
