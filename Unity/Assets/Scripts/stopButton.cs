84e807480ad2211a34d85b22ff86ae623a5f51df
ryo takenoshita <itakedaka@gmail.com>
2014-09-01 01:12:35 +0900

    一時停止機能実装

---
+++ Unity/Assets/Scripts/stopButton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class stopButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3    aTapPoint   = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Collider2D aCollider2d = Physics2D.OverlapPoint(aTapPoint);
+
+		if (aCollider2d) {
+			GameObject obj = aCollider2d.transform.gameObject;
+			if (obj.name == "stopLabel") {
+				    Time.timeScale = 0;
+			} else {
+			    Time.timeScale = 1; // 画面を押すと再開する
+			}
+		}
+	}
+}
